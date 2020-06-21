11e32530090d9f101daf89294fed98685b53ba37
arfaWong <arfa244@gmail.com>
2020-06-21 15:58:10 +0800

    Update OpenWrt-x86-64-Lean-passwall.sh

--- OpenWrt-x86-64-Lean-passwall.sh
+++ OpenWrt-x86-64-Lean-passwall.sh
@@ -29,7 +29,7 @@ rm -rf feeds/packages/net/haproxy && svn co https://github.com/kang-mk/openwrt-a
 rm -rf package/lean/luci-app-haproxy-tcp && svn co https://github.com/kang-mk/openwrt-app-package/trunk/luci-app-haproxy-tcp package/lean/luci-app-haproxy-tcp
 
 # 自定义定制选项
-sed -i 's#192.168.1.1#10.0.0.1#g' package/base-files/files/bin/config_generate #定制默认IP
+sed -i 's#192.168.1.1#10.10.10.253#g' package/base-files/files/bin/config_generate #定制默认IP
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings #取消系统默认密码
 sed -i 's#option commit_interval 24h#option commit_interval 10m#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计写入为10分钟
 sed -i 's#option database_directory /var/lib/nlbwmon#option database_directory /etc/config/nlbwmon_data#g' feeds/packages/net/nlbwmon/files/nlbwmon.config #修改流量统计数据存放默认位置
